82fe151f53c8663a6c2bd8978263ecb1582b89c8
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-02 00:39:09 +0200

    Добавьте файлы проекта.

---
+++ Zadacha 4 3/Zadacha 4 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "write 1 or -1 :";
+    cin >> x;
+
+    if (x == -1) {
+        cout << "Negative number" << endl;
+    }
+    else if (x == 1) {
+        cout << "Positive number" << endl;
+    }
+    else {
+        cout << "error, write 1 or -1" << endl;
+    }
+
+    return 0;
+}
